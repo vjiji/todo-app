99b37321ab49209bad32333a665da0c605f5a384
vjiji <restmi0711@gmail.com>
2024-02-22 01:40:28 +0900

    feat: 메인 페이지의 기본 레이아웃을 구성한다

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="header">
+        <p>To Do List</p>
+        <p>React</p>
+      </div>
+      <div className="add-card__content">
+        <input className="add-card__input"></input>
+        <input className="add-card__input"></input>
+        <button className="add-card__button">추가하기</button>
+      </div>
+      {/* Todo-list */}
+      <p>Working</p>
+      <div className="card">
+        <p>제목</p>
+        <p>내용</p>
+        <button>완료</button>
+        <button>삭제</button>
+      </div>
+      <p>Done</p>
+      <div className="card">
+        <p>제목</p>
+        <p>내용</p>
+        <button>완료</button>
+        <button>삭제</button>
+      </div>
+      {/* Todo-list */}
     </div>
   );
 }
