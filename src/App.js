114a09044568aa9085d7560bea46060d79fc1f8a
vjiji <restmi0711@gmail.com>
2024-02-22 01:56:47 +0900

    feat: 메인페이지의 addTodo 함수를 생성한다

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const addTodo = () => {
+    setTodos([...todos, { title, content, completed: false }]);
+  };
+
   return (
     <div className="app-container">
       <div className="header">
@@ -9,18 +17,34 @@ function App() {
         <p>React</p>
       </div>
       <div className="add-card__content">
-        <input className="add-card__input"></input>
-        <input className="add-card__input"></input>
-        <button className="add-card__button">추가하기</button>
+        <p>제목</p>
+        <input
+          className="add-card__input"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <p>내용</p>
+        <input
+          className="add-card__input"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button className="add-card__button" onClick={addTodo}>
+          추가하기
+        </button>
       </div>
+      <hr />
       {/* Todo-list */}
       <p>Working</p>
-      <div className="card">
-        <p>제목</p>
-        <p>내용</p>
-        <button>완료</button>
-        <button>삭제</button>
-      </div>
+      {todos.map((todo, i) => (
+        <div className="card" key={todo + i}>
+          <p>{todo.title}</p>
+          <p>{todo.content}</p>
+          <button>완료</button>
+          <button>삭제</button>
+        </div>
+      ))}
+
       <p>Done</p>
       <div className="card">
         <p>제목</p>
@@ -31,6 +55,6 @@ function App() {
       {/* Todo-list */}
     </div>
   );
-}
+};
 
 export default App;
