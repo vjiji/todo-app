f7990575b655a58e697de16e87d19e966bd16787
vjiji <restmi0711@gmail.com>
2024-02-22 13:04:18 +0900

    style(card): card의 레이아웃 및 스타일을 적용한다

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,8 +4,8 @@ const Todo = ({ className, todo, completeTodo, deleteTodo }) => {
   const { id, title, content } = todo;
   return (
     <div className={className}>
-      <p>{title}</p>
-      <p>{content}</p>
+      <p className="card-list__card-title">{title}</p>
+      <p className="card-list__card-content">{content}</p>
       <button id={id} onClick={completeTodo}>
         완료
       </button>
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -5,18 +5,28 @@ const Todos = ({ isCompleted, todos, completeTodo, deleteTodo }) => {
   const title = isCompleted ? "Done" : "Working";
 
   return (
-    <>
-      <p>{title}</p>
-      {todos.map((todo) => (
-        <Todo
-          key={todo.title + todo.id}
-          className={"card"}
-          todo={todo}
-          completeTodo={isCompleted ? () => {} : completeTodo}
-          deleteTodo={isCompleted ? () => {} : deleteTodo}
-        />
-      ))}
-    </>
+    <div className="card-list">
+      <p
+        className={`card-list__title ${
+          isCompleted && "card-list__title--completed"
+        }`}
+      >
+        {title}
+      </p>
+      <div className="card-list__card-wrap">
+        {todos.map((todo) => (
+          <Todo
+            key={todo.title + todo.id}
+            className={`card-list__card ${
+              isCompleted && "card-list__card--completed"
+            }`}
+            todo={todo}
+            completeTodo={isCompleted ? () => {} : completeTodo}
+            deleteTodo={isCompleted ? () => {} : deleteTodo}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
