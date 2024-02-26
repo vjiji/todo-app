a58b485c6aea5cf12444ea3790a34ec7a15df094
vjiji <restmi0711@gmail.com>
2024-02-26 17:35:16 +0900

    refactor : todo의 update 및 delete 함수의 매개변수를 수정한다

--- src/components/features/todo/TodoCard.jsx
+++ src/components/features/todo/TodoCard.jsx
@@ -10,13 +10,13 @@ const TodoCard = ({ className, todo, updateTodo, deleteTodo }) => {
         <ActionButton
           className={`card-list__button--${completed ? "cancel" : "complete"}`}
           id={id}
-          onClick={() => updateTodo(id)}
+          onClick={updateTodo}
           text={completed ? "취소하기" : "완료하기"}
         />
         <ActionButton
           className={"card-list__button--delete"}
           id={id}
-          onClick={() => deleteTodo(id)}
+          onClick={deleteTodo}
           text="삭제하기"
         />
       </div>
