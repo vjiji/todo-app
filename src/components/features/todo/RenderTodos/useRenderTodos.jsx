e18a47d40649d1b1d7d765fcf86caa4b0a438532
vjiji <restmi0711@gmail.com>
2024-02-27 12:01:43 +0900

    feat: 리덕스의 updateTodo액션을 생성하고 RenderTodo컴포넌트의 todo완료하기 로직을 수정한다

--- src/components/features/todo/RenderTodos/useRenderTodos.jsx
+++ src/components/features/todo/RenderTodos/useRenderTodos.jsx
@@ -1,17 +1,15 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { updateTodos } from "../../../../redux/modules/todos";
 
 export const useRenderTodos = ({ isCompleted, setTodos }) => {
   const { todos } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
   const handleUpdateTodoButtonClick = (e) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === e.currentTarget.id) {
-          return { ...todo, completed: !todo.completed };
-        }
-        return todo;
-      })
-    );
+    const targetId = e.currentTarget.id;
+    const targetTodo = todos.filter(({ id }) => id === targetId)[0];
+
+    dispatch(updateTodos({ ...targetTodo, completed: !targetTodo.completed }));
   };
 
   const handleDeleteTodoButtonClick = (e) => {
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,12 +1,13 @@
 const READ_TODOS = "READ_TODOS";
 const ADD_TODOS = "ADD_TODOS";
-
-const readTodos = () => {
-  return { type: READ_TODOS };
-};
+const UPDATE_TODOS = "UPDATE_TODOS";
 
 export const addTodos = (payload) => {
   return { type: ADD_TODOS, payload: payload };
+};
+
+export const updateTodos = (payload) => {
+  return { type: UPDATE_TODOS, payload: payload };
 };
 
 const initialState = {
@@ -19,7 +20,16 @@ const todos = (state = initialState, action) => {
       return {
         todos: [
           ...state.todos,
-          { ...action.payload, id: state.todos.length + 1 },
+          { ...action.payload, id: String(state.todos.length + 1) },
+        ],
+      };
+
+    case UPDATE_TODOS:
+      return {
+        todos: [
+          ...state.todos.map((todo) =>
+            todo.id === action.payload.id ? action.payload : todo
+          ),
         ],
       };
 
