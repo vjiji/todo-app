6dd228ec1e9b88b3a4933ddf8f07114cd911a238
vjiji <restmi0711@gmail.com>
2024-02-27 16:12:41 +0900

    refactor: AddTodo컴포넌트의 스타일 코드를 styled-components를 사용해 적용한다

--- src/components/common/TextInput.jsx
+++ src/components/common/TextInput.jsx
@@ -1,5 +1,15 @@
-const TextInput = ({ className, value, onChange }) => {
-  return <input className={className} value={value} onChange={onChange} />;
+import styled from "styled-components";
+
+const TextInput = ({ value, onChange }) => {
+  return <InputStyle value={value} onChange={onChange} />;
 };
 
 export default TextInput;
+
+const InputStyle = styled.input`
+  width: 220px;
+  height: 24px;
+  margin-left: 10px;
+  border: none;
+  border-radius: 4px;
+`;
