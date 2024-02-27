33891fb8477e1a4b32a043cde8ad7053ddc7311f
vjiji <restmi0711@gmail.com>
2024-02-27 13:54:57 +0900

    feat: url파라미터의 id값으로 해당 todo의 상세정보를 조회해 렌더링한다

--- src/pages/TodoDetail/TodoDetail.jsx
+++ src/pages/TodoDetail/TodoDetail.jsx
@@ -1,12 +1,17 @@
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 const TodoDetail = () => {
-  const param = useParams();
-  const todoId = param.id;
+  const currentId = useParams().id;
+  const { todos } = useSelector((state) => state.todos);
+  const todo = todos.filter(({ id }) => id === currentId)[0];
   return (
     <>
       <div>TodoDetail</div>
-      <p>{todoId}</p>
+      <p>{todo.id}</p>
+      <p>{todo.title}</p>
+      <p>{todo.content}</p>
+      <p>{todo.completed ? "완료" : "대기"}</p>
     </>
   );
 };
